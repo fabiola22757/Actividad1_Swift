adde59769612140a1619e4789a0c252ebda6838b
Fabiola <fabiola.juarez>
2025-08-18 17:51:26 -0600

    Initial Commit

---
+++ FuenteIdioma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FuenteIdioma
+//
+//  Created by win603 on 18/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FuenteIdioma/FuenteIdiomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuenteIdiomaApp.swift
+//  FuenteIdioma
+//
+//  Created by win603 on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FuenteIdiomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
