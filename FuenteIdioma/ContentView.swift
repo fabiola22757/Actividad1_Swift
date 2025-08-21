f1c69940d6bc141696f266413bf88d8bea39ce8e
Fabiola <fabiola.juarez>
2025-08-20 18:17:11 -0600

    Agregados Localizable en español e inglés

--- FuenteIdioma/ContentView.swift
+++ FuenteIdioma/ContentView.swift
@@ -7,14 +7,17 @@
 
 import SwiftUI
 
-let starwarstext = "¡GUERRA! La República está en declive. Bajo el ataque del despiadado Lord Sith, Conde Dooku. Hay héroes en ambos lados. El mal está por todas partes. En un movimiento asombroso, el líder droide, General Grievous, ha irrumpido en la capital de la República y ha secuestrado al Canciller Palpatine, líder del Senado Galáctico. Mientras el ejército droide separatista intenta escapar de la asediada capital con su valioso rehén, dos caballeros Jedi lideran una misión desesperada para rescatar al Canciller cautivo..."
-
 struct ContentView: View {
+    //Constantes lenguaje
+    let episodio: String = String(localized: "episode")
+    let titulo: String = String(localized: "title")
+    let descripcion: String = String(localized: "description")
+    
     var body: some View {
         VStack{
-            Text("Episodio 3")
-            Text("La Venganza de los Sith")
-            Text(starwarstext)
+            Text(episodio)
+            Text(titulo)
+            Text(descripcion)
                 .rotation3DEffect(.degrees(45), axis: (x: 1, y: 0, z: 0))
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity) //frame
